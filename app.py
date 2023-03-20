8537e7f810a577121cd28e9ea7cd2140a0ac5bdc
Retr0V1z0r <50799328+Br0fer@users.noreply.github.com>
2023-03-21 00:18:04 +0200

    Fourth Commit
    Functional🥳🥳🥳

--- app.py
+++ app.py
@@ -5,18 +5,7 @@ app = Flask(__name__)  # Створюємо вебдодаток Flask
 
 @app.route("/")  # Вказуємо url-адресу для виклику функції
 def index():
-    return render_template("index.html")  # Результат, що повертається у браузер
-
-
-@app.route("/home")
-def home():
-    return render_template("my_page.html")
-
-
-@app.route("/home/first")
-def first():
-    return render_template("first.html")
-
+    return render_template("my_page.html")  # Результат, що повертається у браузер
 
 if __name__ == "__main__":
     app.config['TEMPLATES_AUTO_RELOAD'] = True
