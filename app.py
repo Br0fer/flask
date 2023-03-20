44c3e597a2598724b3301cc550bae7bc88be4f09
Retr0V1z0r <50799328+Br0fer@users.noreply.github.com>
2023-03-20 20:11:59 +0200

    Third Commit

--- app.py
+++ app.py
@@ -1,15 +1,23 @@
 from flask import Flask, render_template
 
-app = Flask(__name__) # Створюємо веб–додаток Flask
+app = Flask(__name__)  # Створюємо вебдодаток Flask
 
-@app.route("/") # Вказуємо url-адресу для виклику функції
+
+@app.route("/")  # Вказуємо url-адресу для виклику функції
 def index():
-    return render_template("index.html")#Результат, що повертається у браузер
+    return render_template("index.html")  # Результат, що повертається у браузер
+
 
 @app.route("/home")
 def home():
     return render_template("my_page.html")
 
+
+@app.route("/home/first")
+def first():
+    return render_template("first.html")
+
+
 if __name__ == "__main__":
     app.config['TEMPLATES_AUTO_RELOAD'] = True
-    app.run(port=5001, debug=True) # Запускаємо веб-сервер з цього файлу
+    app.run(port=5001, debug=True)  # Запускаємо веб-сервер з цього файлу
