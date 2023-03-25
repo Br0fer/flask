78f4dc9593eabbc541f74563fee911176f3bc56b
Retr0V1z0r <50799328+Br0fer@users.noreply.github.com>
2023-03-25 18:53:17 +0200

    (I didn't count) Commit
    Nothing.... yeh just nothing

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ app = Flask(__name__)  # Створюємо вебдодаток Flask
 def index():
     return render_template("my_page.html")  # Результат, що повертається у браузер
 
+
 if __name__ == "__main__":
     app.config['TEMPLATES_AUTO_RELOAD'] = True
     app.run(port=5001, debug=True)  # Запускаємо веб-сервер з цього файлу
