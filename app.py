dc203c7bcd8513b9ef89bf1d670c0d6d586fea70
Retr0V1z0r <50799328+Br0fer@users.noreply.github.com>
2023-03-25 19:16:05 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ app = Flask(__name__)  # Створюємо вебдодаток Flask
 
 @app.route("/")  # Вказуємо url-адресу для виклику функції
 def index():
-    return render_template("my_page.html")  # Результат, що повертається у браузер
+	return render_template("my_page.html")  # Результат, що повертається у браузер
 
 
 if __name__ == "__main__":
-    app.config['TEMPLATES_AUTO_RELOAD'] = True
-    app.run(port=5001, debug=True)  # Запускаємо веб-сервер з цього файлу
+	app.config['TEMPLATES_AUTO_RELOAD'] = True
+	app.run(port=5001, debug=True)  # Запускаємо веб-сервер з цього файлу
