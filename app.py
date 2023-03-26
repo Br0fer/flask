baaf9d0826a5da5de6ff5d355c65d974c875972b
Retr0V1z0r <50799328+Br0fer@users.noreply.github.com>
2023-03-26 12:30:06 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,30 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
+db = SQLAlchemy()
 app = Flask(__name__)  # Створюємо вебдодаток Flask
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///recepies.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db.init_app(app)
+
+
+class User(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	username = db.Column(db.String(50), unique = True, nullable = False)
+	password = db.Column(db.String, nullable = False)
+	name = db.Column(db.String(200))
+	logiks = db.Column(db.Integer, default = 0)
+
+	projects = db.relationship('Project', backref = 'author')
+
+
+class Project(db.Model):
+	id=db.Column(db.Integer, primary_key = True)
+	title = db.Column(db.String, nullable = False)
+	description = db.Column(db.Text)
+	link = db.Column(db.Text)
+	
+	user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 
 @app.route("/")  # Вказуємо url-адресу для виклику функції
