651354fee7d9077a9d0fd251335538b53c38319b
Retr0V1z0r <50799328+Br0fer@users.noreply.github.com>
2023-04-01 18:56:21 +0300

    URAAAAAAAAA

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+
+from flask import Flask, render_template
 
 db = SQLAlchemy()
 app = Flask(__name__)  # Створюємо вебдодаток Flask
@@ -7,30 +8,42 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///recepies.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db.init_app(app)
 
-
 class User(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	username = db.Column(db.String(50), unique = True, nullable = False)
 	password = db.Column(db.String, nullable = False)
 	name = db.Column(db.String(200))
-	logiks = db.Column(db.Integer, default = 0)
 
-	projects = db.relationship('Project', backref = 'author')
+	recepies = db.relationship('Recepies', backref = 'author')
 
+	def __repr__(self):
+		return f"User: {self.username}"
 
-class Project(db.Model):
+class Recepies(db.Model):
 	id=db.Column(db.Integer, primary_key = True)
 	title = db.Column(db.String, nullable = False)
-	description = db.Column(db.Text)
+	ingredients = db.Column(db.Text)
+	actions = db.Column(db.Text)
 	link = db.Column(db.Text)
 	
 	user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-
+	
+	def __repr__(self):
+		return f"Recepie: {self.title}"
 
 @app.route("/")  # Вказуємо url-адресу для виклику функції
 def index():
-	return render_template("my_page.html")  # Результат, що повертається у браузер
+	recepies = Recepies.query.all()
+	print(recepies)
+	return render_template("my_page.html", recepies=recepies)  # Результат, що повертається у браузер
 
+@app.route("/signup", methods = ["POST", "GET"])
+def signup():
+	return render_template("signup.html")
+
+@app.route("/signin", methods = ["POST", "GET"])
+def signin():
+	return render_template("login.html")
 
 if __name__ == "__main__":
 	app.config['TEMPLATES_AUTO_RELOAD'] = True
--- testdb.py
+++ testdb.py
@@ -1,4 +1,21 @@
 from app import *
 
 with app.app_context():
-    db.create_all()
+	db.create_all()
+	user = User(name='Вова', username = 'bvodfjkfkdf', password = '23bgf232')
+	recepie = Recepies(title = "Картопляні кльоцки з беконом",
+		ingredients = '''
+		﻿﻿✔️картопля 500 г
+		﻿﻿✔️яйце 1 шт
+		﻿﻿✔️борошно 100 г
+		﻿﻿✔️цибуля 1 шт.
+		﻿﻿✔️бекон
+		﻿﻿✔️сіль, перець за смаком''',
+		actions = '''Картоплю відварюємо, чистимо і натираємо на дрібній терці. Додаємо яйце, борошно та спеції, замішуємо тісто. Ділимо на 3 частинки, формуємо ковбаски і робимо кльоцки, як на відео.
+		Кидаємо кльоцки в киплячу воду, варимо поки не вспливуть.
+		Обсмажуємо цибульку з беконом, викладаємо на кльоцки.''',
+		link = "https://t.me/+wW5yXPQcKoE4OWM6")
+	db.session.add(user)
+	db.session.add(recepie)
+	db.session.commit()
+
